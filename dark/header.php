01e8d076d2229289f63847f6ffb7840aa5b51737
KH9-hack-PC <909Passhack@gmail.com>
2021-12-25 20:13:56 +0400

    first commit

---
+++ dark/header.php
@@ -0,0 +1,46 @@
+<?php
+?>
+<html>
+  <head>
+    <title>=)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/style.css">
+  </head>
+  <body>
+   
+    <div class="container">
+    <header>
+  <nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="../images/Screenshot 2021-12-13 173646.png" alt="=("></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link active" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Privacy</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="textcenter1">
+  <h2>Free OnePage Theme</h2>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, minima quis, fuga assumenda nemo voluptas illum nostrum vitae enim velit quo excepturi vel fugiat nulla aliquid, nobis aliquam consequuntur libero.</p>
+</div>
+</header>
+<?php
+?>
---
+++ dark/index.php
@@ -0,0 +1,100 @@
+<?php require_once "header.php"
+?>
+<div class="devlopments">
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="ios-devlopment">
+                <img width="100px" height="100px" src="../images/apple.jpg" alt="=)">
+                <h2>IOS Devlopment</h2>
+                <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>In quibusdam deserunt
+                    consequatur perferendis,<br>animi eos neque magni modi dolor asperiores perspiciatis non corrupti?
+                    Excepturi,<br>non architecto. Ratione iusto suscipit veritatis.</p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="ios-devlopment">
+                <img width="100px" height="100px" src="../images/android.png" alt="=)">
+                <h2>Android Devlopment</h2>
+                <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>In quibusdam deserunt
+                    consequatur perferendis,<br>animi eos neque magni modi dolor asperiores perspiciatis non corrupti?
+                    Excepturi,<br>non architecto. Ratione iusto suscipit veritatis.</p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="ios-devlopment">
+                <img width="100px" height="100px" src="../images/windows.png" alt="=)">
+                <h2>Windows Phone Devlopment</h2>
+                <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>In quibusdam deserunt
+                    consequatur perferendis,<br>animi eos neque magni modi dolor asperiores perspiciatis non corrupti?
+                    Excepturi,<br>non architecto. Ratione iusto suscipit veritatis.</p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="ios-devlopment">
+                <img width="100px" height="100px" src="../images/html.png" alt="=)">
+                <h2>HTML Devlopment</h2>
+                <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>In quibusdam deserunt
+                    consequatur perferendis,<br>animi eos neque magni modi dolor asperiores perspiciatis non corrupti?
+                    Excepturi,<br>non architecto. Ratione iusto suscipit veritatis.</p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="ios-devlopment">
+                <img width="100px" height="100px" src="../images/js.jpg" alt="=)">
+                <h2>JS Devlopment</h2>
+                <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>In quibusdam deserunt
+                    consequatur perferendis,<br>animi eos neque magni modi dolor asperiores perspiciatis non corrupti?
+                    Excepturi,<br>non architecto. Ratione iusto suscipit veritatis.</p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="ios-devlopment">
+                <img width="100px" height="100px" src="../images/like.png" alt="=)">
+                <h2>Responsive and Website Design</h2>
+                <p class="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>In quibusdam deserunt
+                    consequatur perferendis,<br>animi eos neque magni modi dolor asperiores perspiciatis non corrupti?
+                    Excepturi,<br>non architecto. Ratione iusto suscipit veritatis.</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="porfolio">
+        <h2 class="h21 text-center">
+            Portfolio
+        </h2>
+        <p class="p2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit consectetur molestiae ab, fugiat
+            iste nemo provident voluptate accusamus illum dolorum architecto soluta ullam maiores laudantium blanditiis,
+            accusantium, laboriosam sed. Consequatur.</p>
+        <div class="d-flex buttons">
+            <button class="buton1">All</button>
+            <button class="buton1">Design</button>
+            <button class="buton1">HTML</button>
+            <button class="buton1">WorkSpace</button>
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                <img width="350px" height="300px" src="../images/1.jpg" alt="=)">
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                <img width="350px" height="300px" src="../images/2.webp" alt="=)">
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                <img width="350px" height="300px" src="../images/3.jpg" alt="=)">
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+                <img width="350px" height="300px" src="../images/4.jpg" alt="=)">
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+</script>
+<?php
+?>
